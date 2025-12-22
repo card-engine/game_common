5ed4233ce04208d2148af591cee8586f34d70fbf
cooper <cooper@gmail.com>
2025-12-22 14:27:53 +0800

    bet config code

---
+++ api/history/client/game_bet_config_call.go
@@ -0,0 +1,14 @@
+package client
+
+import (
+	"context"
+	v1 "github.com/card-engine/game_common/api/history/v1"
+
+	google_grpc "google.golang.org/grpc"
+)
+
+// 获取游戏下注配置详情
+func GetGameBetConfig(ctx context.Context, grpcClient *google_grpc.ClientConn, req *v1.GetGameBetConfigRequest) (*v1.GetGameBetConfigReply, error) {
+	client := v1.NewGameBetConfigApiClient(grpcClient)
+	return client.GetGameBetConfig(ctx, req)
+}
